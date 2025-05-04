c2a9569503953a790807edf3bdea2312e8c7d5e7
SureshRajaMH <sselvadurai@selectdr.com>
2025-05-04 14:07:47 -0700

    - dev, qa , prod push

--- deploy/secret.sh
+++ deploy/secret.sh
@@ -9,9 +9,9 @@ kubectl create secret generic job-secrets \
   --from-literal=DB_PORT=3306 \
   --from-literal=DB_NAME=jdatabase \
   --from-literal=SLACK_WEBHOOK_URL=https://hooks.slack.com/services/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 \
-  --from-literal=NOTIFIER_URL=http://notifier.sureshraja.live \
-  --from-literal=BACKEND_URL=http://backend.sureshraja.live \
-  --from-literal=FRONTEND_URL=http://app.sureshraja.live
+  --from-literal=NOTIFIER_URL=https://dev.notifier.sureshraja.live \
+  --from-literal=BACKEND_URL=https://dev.backend.sureshraja.live \
+  --from-literal=FRONTEND_URL=https://dev.app.sureshraja.live
 
 kubectl apply -f infra/dev/job-backend/deployment-green.yaml
 
@@ -28,3 +28,7 @@ kubectl apply -f infra/qa/job-backend/deployment-blue.yaml \
               -f infra/qa/job-notifier/deployment-blue.yaml \
               -f infra/qa/job-notifier/deployment-green.yaml \
               -f infra/qa/job-notifier/service.yaml
+
+
+kubectl get pods -l app=job-backend -o wide
+kubectl exec -it job-backend-blue-6fb4bb96b8-ktrs2  -- /bin/sh
